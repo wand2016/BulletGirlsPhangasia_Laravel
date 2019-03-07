dc4e7b0cecb1632599a549d6fe3bb10cbff6b437
wand <d19921207@gmail.com>
2019-03-07 23:32:28 +0900

    update deployment environment

--- deploy/ecr_register_images.sh
+++ deploy/ecr_register_images.sh
@@ -1,6 +1,6 @@
 # login ecr
 # CRが含まれるのでsedで除く
-docker run --rm -it \
+docker container run --rm -it \
        -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
        -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
        -e DEFAULT_REGION_NAME=$DEFAULT_REGION_NAME \
@@ -9,14 +9,10 @@ docker run --rm -it \
     | sed --expression='s/\r$//' \
     | sh
 
-
-docker image pull registry:${REGISTRY_PORT}/deploy/nginx:latest
-docker image pull registry:${REGISTRY_PORT}/deploy/php-fpm:latest
-
 # push nginx image
-docker tag registry:${REGISTRY_PORT}/deploy/nginx:latest ${AWS_ECR_HOST}/bgp/nginx:latest
-docker push ${AWS_ECR_HOST}/bgp/nginx:latest
+docker image tag ${COMPOSE_PROJECT_NAME}_nginx ${AWS_ECR_HOST}/bgp/nginx:latest
+docker image push ${AWS_ECR_HOST}/bgp/nginx:latest
 
 # push php-fpm image
-docker tag registry:${REGISTRY_PORT}/deploy/php-fpm:latest ${AWS_ECR_HOST}/bgp/php-fpm:latest
-docker push ${AWS_ECR_HOST}/bgp/php-fpm:latest
+docker image tag ${COMPOSE_PROJECT_NAME}_php-fpm_deploy ${AWS_ECR_HOST}/bgp/php-fpm:latest
+docker image push ${AWS_ECR_HOST}/bgp/php-fpm:latest
