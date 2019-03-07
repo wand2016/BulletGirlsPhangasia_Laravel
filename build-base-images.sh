7b5c2eac7608946635f52a515ac1297f95a3e9b5
wand <d19921207@gmail.com>
2019-03-07 22:56:07 +0900

    delete unused files

--- build-base-images.sh
+++
@@ -1,9 +0,0 @@
-docker-compose -f base-image-builder.yml build
-
-
-# ブレース展開したい人生だった
-docker image push registry:${REGISTRY_PORT}/base/nginx
-docker image push registry:${REGISTRY_PORT}/base/php-fpm
-docker image push registry:${REGISTRY_PORT}/base/mysql
-docker image push registry:${REGISTRY_PORT}/base/node
-docker image push registry:${REGISTRY_PORT}/base/composer
--- build.sh
+++
@@ -1,4 +0,0 @@
-docker-compose -f docker-compose-deploy.yml build
-
-docker image push registry:${REGISTRY_PORT}/deploy/nginx:latest
-docker image push registry:${REGISTRY_PORT}/deploy/php-fpm:latest
